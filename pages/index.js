3f02fb1c717bb8d537eabbd4ed389daaa361f823
alejandro gonzalez flores <agf_001@outlook.com>
2026-07-12 18:57:04 -0700

    Remaquetando con Bulma

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function main() {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of our application.</p>
+      <h1>Hola Mundo</h1>
     </div>
   );
+    
 }
 main();
